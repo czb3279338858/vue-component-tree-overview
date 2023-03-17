6b7a396785661e0e27723ba095d3a7f5d10ba75a
蔡朝勃 <3279338858@qq.com>
2023-03-17 16:00:11 +0800

    1

--- vue-overview-loader/src/index.js
+++ vue-overview-loader/src/index.js
@@ -4,14 +4,17 @@
  */
 /** ast 解析器 */
 const { parseForESLint } = require('vue-eslint-parser')
-/** ast 遍历器 */
-const estraverse = require("estraverse");
 /** eslint-plugin-vue 辅助创建 visitor 对象的工具 */
 const utils = require('eslint-plugin-vue/lib/utils')
+const Traverser = require('eslint/lib/shared/traverser.js')
+const createEmitter = require('eslint/lib/linter/safe-emitter.js')
+const CodePathAnalyzer = require("eslint/lib/linter/code-path-analysis/code-path-analyzer.js")
+const NodeEventGenerator = require("eslint/lib/linter/node-event-generator.js")
 
 module.exports = function loader(source) {
 	const { loaders, resource, request, version, webpack } = this;
 	console.log('vue-overview-loader');
+	/** 解析为 ast */
 	const sourceCode = parseForESLint(source, {
 		"ecmaVersion": 2020,
 		"sourceType": "module",
@@ -23,23 +26,77 @@ module.exports = function loader(source) {
 			"jsx": true
 		}
 	})
-	const visitor = utils.defineTemplateBodyVisitor({
+
+	const ruleListeners = utils.defineTemplateBodyVisitor({
 		parserServices: sourceCode.services
 	}, {
+		// 标签开始
 		VStartTag() {
 			console.log('VStartTag')
 		},
+		// 属性
 		VAttribute(node) {
 			console.log('VAttribute')
 
 		}
 	})
 
-	estraverse.traverse(sourceCode.ast, {
+
+	const nodeQueue = [];
+	Traverser.traverse(sourceCode.ast, {
+		enter(node, parent) {
+			node.parent = parent;
+			nodeQueue.push({ isEntering: true, node });
+		},
 		leave(node) {
-			visitor['Program:exit'](node)
+			nodeQueue.push({ isEntering: false, node });
+		},
+		visitorKeys: sourceCode.visitorKeys
+	});
+
+
+
+	function addRuleErrorHandler(ruleListener) {
+		return function ruleErrorHandler(...listenerArgs) {
+			try {
+				return ruleListener(...listenerArgs);
+			} catch (e) {
+				e.ruleId = ruleId;
+				throw e;
+			}
+		};
+	}
+
+	/** 事件对象 */
+	const emitter = createEmitter();
+
+	Object.keys(ruleListeners).forEach(selector => {
+		emitter.on(
+			selector,
+			addRuleErrorHandler(ruleListeners[selector])
+		);
+	});
+
+	const eventGenerator = nodeQueue[0].node.type === "Program"
+		? new CodePathAnalyzer(new NodeEventGenerator(emitter, { visitorKeys: sourceCode.visitorKeys, fallback: Traverser.getKeys }))
+		: new NodeEventGenerator(emitter, { visitorKeys: sourceCode.visitorKeys, fallback: Traverser.getKeys });
+
+	nodeQueue.forEach(traversalInfo => {
+		currentNode = traversalInfo.node;
+
+		try {
+			if (traversalInfo.isEntering) {
+				eventGenerator.enterNode(currentNode);
+			} else {
+				eventGenerator.leaveNode(currentNode);
+			}
+		} catch (err) {
+			err.currentNode = currentNode;
+			throw err;
 		}
-	})
+	});
+
+
 	const newSource = `
 	/**
 	 * vue-overview-loader
