ba6a8c45acae39ae8664b6f8ebd8f329a73e7f80
蔡朝勃 <3279338858@qq.com>
2023-03-20 17:30:25 +0800

    1

--- 2.0/src/views/HomeView.vue
+++ 2.0/src/views/HomeView.vue
@@ -1,6 +1,10 @@
+<!-- 组件备注1 -->
+<!-- 组件备注2 -->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- 标签备注1 -->
+    <!-- 标签备注2 -->
+    <img alt="Vue logo" src="../assets/logo.png" :attr-some="dataAttr">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -13,6 +17,9 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
   components: {
     HelloWorld,
   },
+  data:{
+    dataAttr:1
+  }
 })
 export default class HomeView extends Vue {}
 </script>
