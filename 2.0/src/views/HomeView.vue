4404bf3b4a90286cc57c2dac5593947c022f2f30
蔡朝勃 <3279338858@qq.com>
2023-03-28 17:28:48 +0800

    1

--- 2.0/src/views/HomeView.vue
+++ 2.0/src/views/HomeView.vue
@@ -1,28 +1,47 @@
-<!-- 组件备注1 -->
-<!-- 组件备注2 --><template>
-    <div>
-      <span>111</span>
-      <!-- 标签备注1 -->
-      <!-- 标签备注2 -->
-      <HelloWorld v-on="a" v-on:style="a" v-on:c.native="a" @="a" @blur="a" @blur.native="a"/>
-    </div>
+<!-- 测试1 -->
+<template>
+  <div1 :class="aaa.a" :style="a">
+    <div2 v-for="val in a" :key="val">333</div2>
+    <!-- 测试2 -->
+    123
+    <div3>
+      <span :value="name">
+        123
+        {{ index }}
+        <!--111-->
+        {{ val.id }}
+        {{ val.id + index }}
+        {{ `${val.id}${index}` }}
+        {{ doSome(val) }}
+      </span>
+
+      <span>
+        <!-- 测试 -->
+        123 456
+      </span>
+    </div3>
+  </div1>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import { Component, Prop, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// 测试3
+interface ABC {
+  a: string;
+}
 @Component({
   components: {
     HelloWorld,
-  }
+  },
 })
 export default class HomeView extends Vue {
-  a={}
-  b=2
-  c(){
-    console.log('c')
-  }
-  d={}
+  @Prop({
+    default: () => {
+      return [];
+    },
+    required: true,
+  })
+  readonly e!: ABC[];
 }
 </script>
--- vue-overview-loader/src/index.js
+++ vue-overview-loader/src/index.js
@@ -6,6 +6,8 @@ const { Linter } = require('eslint')
 const { parseForESLint } = require('vue-eslint-parser')
 const typescriptEslintParser = require('@typescript-eslint/parser')
 const utils = require('eslint-plugin-vue/lib/utils/index')
+const ts = require('typescript')
+
 module.exports = function loader(source) {
 	const { loaders, resource, request, version, webpack } = this;
 	const linter = new Linter()
@@ -26,10 +28,22 @@ module.exports = function loader(source) {
 	})
 	linter.defineRule("my-rule", {
 		create(context) {
+			const sourceCode = context.getSourceCode()
+
+			// TODO:获取属性的key，这个方法暂时由sourceCode.getText(a.key)代替，以后看看是否扩充为支持动态绑定 v-on:[event]="doThis"
 			function getAttributesName(attributeKey) {
 				if (attributeKey.rawName) return attributeKey.rawName
 				const { name, argument, type, modifiers } = attributeKey
-				const keyName = `${name.rawName}${name.rawName === ':' ? '' : ':'}${argument ? argument.rawName : ''}${modifiers ? modifiers.map(m => `.${m.rawName}`) : ''}`
+				let keyName = name.rawName
+				if (argument) {
+					// 属性中有参数，例如`:style="styleName"`中的style
+					// 动态事件和绑定v-on:[event]="doThis"、v-bind:[key]="value"直接显示文本
+					const argumentName = argument ? (argument.rawName ? argument.rawName : sourceCode.getText(argument)) : undefined
+					keyName = argumentName ? (keyName === ':' ? `${keyName}${argumentName}` : `${keyName}:${argumentName}`) : keyName
+				}
+				if (modifiers) {
+					keyName = `${keyName}${modifiers.map(m => `.${m.rawName}`)}`
+				}
 				// 指令
 				if (type === 'VDirectiveKey' && ![':', '@'].includes(name.rawName)) {
 					return `v-${keyName}`
@@ -37,45 +51,83 @@ module.exports = function loader(source) {
 				return keyName
 			}
 			function getAttributesValue(attributeValue) {
-				if (attributeValue.value) return [attributeValue.value, attributeValue.type]
-				return [attributeValue.expression.name, attributeValue.expression.type]
+				// 只有属性没有值时，例如 v-else
+				if (!attributeValue) return []
+
+				// 绑定值是常量，数值、字符串、布尔值，[常量文本,'VLiteral']
+				if (attributeValue.type === 'VLiteral') return [attributeValue.value, attributeValue.type]
+
+				// 绑定值是变量，[函数名、变量名,'Identifier']
+				if (attributeValue.expression.Identifier === 'Identifier') return [attributeValue.expression.name, attributeValue.expression.type]
+
+				// 绑定值是函数调用,[函数名,'CallExpression']
+				// 函数调用可以层层嵌套的，这里不支持嵌套，只读取第一层的函数名
+				if (attributeValue.expression.type === 'CallExpression') {
+					return [attributeValue.expression.callee.name, attributeValue.expression.type]
+				}
+
+				// 绑定值是v-for的值，[遍历的对象，'VForExpression'，遍历的项名[]]
+				// 遍历的对象只支持变量
+				// 遍历的项名[]，例如 (val, name, index) in object => [val,name,index]
+				if (attributeValue.expression.type === 'VForExpression') {
+					return [attributeValue.expression.right.type === 'Identifier' ? attributeValue.expression.right.name : undefined, attributeValue.expression.type, attributeValue.expression.left.map(l => l.name)]
+				}
+
+				// 表达式直接返回文本
+				// 包括:attr="'string'" 这个时候返回["'string'",'VExpressionContainer']
+				if (attributeValue.expression.type === 'VExpressionContainer') {
+					return [sourceCode.getText(attributeValue), attributeValue.expression.type]
+				}
+
+				// 其他，好像没其他了
+				return [undefined, attributeValue.expression.type]
 			}
 			/** 所有的template中的注释都在template节点中 */
-			function getElementAllComments(element) {
+			function getAllComments(element) {
 				if (element.comments) return element.comments
-				return getElementAllComments(element.parent)
+				return getAllComments(element.parent)
 			}
-			/** 根据当前标签代码所在行和列以及上一级（父级或兄弟上级）的行和列来查找当前节点的注释节点 */
-			function getElementComments(element) {
-				const comments = getElementAllComments(element)
-				const parent = element.parent
+			function isEmptyVText(node) {
+				return node.type === 'VText' && /^[\n\s]*$/.test(node.value)
+			}
+			/** 
+			 * 根据当前节点所在行和列以及上一级（父级或兄弟上级）的行和列来查找当前节点的注释节点 
+			 * 注意，上一级有可能是个VElement它需要获取开始标签来做判断
+			 * node不允许使用startTag，应该传如它的VElement
+			 * */
+			function getTemplateCommentsBefore(node) {
+				const comments = getAllComments(node)
+				const parent = node.parent
 				const parentChildren = parent.children
-				const { line: nodeStartLine, column: nodeStartColumn } = element.startTag.loc.start
+				const startNode = node.type === 'VElement' ? node.startTag : node
+				const { line: nodeStartLine, column: nodeStartColumn } = startNode.loc.start
 
 				const reverseParentChildren = [...parentChildren].reverse()
+
 				let preNode
+				// 在子级中查找同级的上一个兄弟元素
 				for (const index in reverseParentChildren) {
 					const c = reverseParentChildren[index]
-					if (preNode === null && !(c.type === 'VText' && /\n+/.test(c.value))) {
+					if (preNode === null && !(isEmptyVText(c))) {
 						preNode = c
 						break;
 					}
-					if (c === element) preNode = null
+					if (c === node) preNode = null
 				}
 				let preNodeEndLine
 				let preNodeEndColumn
-				// 有上一个兄弟元素
+				// 有同级的上一个兄弟元素
 				if (preNode) {
 					preNodeEndLine = preNode.loc.end.line
 					preNodeEndColumn = preNode.loc.end.column
 				} else {
-					// 文件的顶层节点
+					// template节点
 					if (!parent.parent) {
 						preNodeEndLine = 1
 						preNodeEndColumn = 0
 					} else {
-						preNodeEndLine = parent.startTag.loc.end.line
-						preNodeEndColumn = parent.startTag.loc.end.column
+						preNodeEndLine = parent.type === 'VElement' ? parent.startTag.loc.end.line : parent.loc.end.line
+						preNodeEndColumn = parent.type === 'VElement' ? parent.startTag.loc.end.column : parent.loc.end.column
 					}
 				}
 				let nodeComments = []
@@ -90,41 +142,117 @@ module.exports = function loader(source) {
 				}
 				return nodeComments
 			}
-			const elementMap = new Map()
-			return utils.defineTemplateBodyVisitor(
-				context,
-				{
-					VStartTag(startTag) {
-						const element = startTag.parent
-						const elementName = element.rawName
-						const attributes = startTag.attributes.map(a => {
-							const name = getAttributesName(a.key)
-							const [valueName, valueType] = getAttributesValue(a.value)
-							return {
-								name,
-								valueName,
-								valueType
-							}
-						})
-						const elementComments = getElementComments(element)
-						const elementComment = elementComments.reduce((p, c) => {
-							p = p ? `${p}\n${c.value}` : c.value
-							return p
-						}, '')
-						const elementInfo = {
-							elementName,
-							attributes,
-							elementComment,
-							children: []
-						}
-						const parent = element.parent
-						const parentInfo = elementMap.get(parent)
-						if (parentInfo) {
-							parentInfo.children.push(elementInfo)
-						}
-						elementMap.set(element, elementInfo)
-					},
+			function getTemplateCommentBefore(node) {
+				const elementComments = getTemplateCommentsBefore(node)
+				const elementComment = elementComments.reduce((p, c) => {
+					p = p ? `${p}\n${c.value}` : c.value
+					return p
+				}, '')
+				return elementComment
+			}
+			function addTemplateMap(node, elementInfo) {
+				const parent = node.parent
+				const parentInfo = templateMap.get(parent)
+				if (parentInfo) {
+					parentInfo.children.push(elementInfo)
+				} else {
+					rootTemplate.add(elementInfo)
 				}
+				templateMap.set(node, elementInfo)
+			}
+			const rootTemplate = new Set()
+			const templateMap = new Map()
+
+			return utils.compositingVisitors(
+				utils.defineTemplateBodyVisitor(
+					context,
+					{
+						// 标签
+						VElement(element) {
+							const templateValue = element.rawName
+							// TODO：v-for的循环值被绑定后无法和其他值区分出来
+							const attributes = element.startTag.attributes.map(a => {
+								const name = sourceCode.getText(a.key)
+								// const name = getAttributesName(a.key)
+								const [valueName, valueType, vForLeft] = getAttributesValue(a.value)
+								return {
+									name,
+									valueName,
+									valueType,
+									vForLeft
+								}
+							})
+							const templateComment = getTemplateCommentBefore(element)
+							const templateInfo = {
+								templateValue,
+								templateType: 'VElement',
+								attributes,
+								templateComment,
+								children: []
+							}
+							addTemplateMap(element, templateInfo)
+						},
+						// 标签内文本
+						'VElement>VText'(node) {
+							if (isEmptyVText(node)) return
+							const templateInfo = {
+								templateValue: node.value,
+								templateType: 'VText',
+								attributes: [],
+								templateComment: getTemplateCommentBefore(node),
+								children: []
+							}
+							addTemplateMap(node, templateInfo)
+						},
+						// 不支持在{{ }}内部写 // 注释，只支持在{{ }}之前
+						// 只支持变量、函数调用、
+						'VElement>VExpressionContainer'(node) {
+							const templateInfo = {
+								templateName: sourceCode.getText(node.expression),
+								templateType: 'VExpressionContainer',
+								attributes: [],
+								templateComment: getTemplateCommentBefore(node),
+								children: []
+							}
+
+						}
+					},
+					// script 解析
+					// {
+					// 	// class component @Prop
+					// 	'Decorator[expression.callee.name=Prop]'(node) {
+					// 		const prop = node.parent
+					// 		const propName = prop.key.name
+					// 		const decoratorArgument = node.expression.arguments[0]
+					// 		let { default: propDefault, type: propType, required: propRequired } = decoratorArgument.properties.reduce((p, c) => {
+					// 			// TODO:default如果是个函数需要获取函数返回值，这里暂不支持
+					// 			if (c.key.name === 'default') p['default'] = sourceCode.getText(c.value)
+					// 			if (c.key.name === 'type') p['type'] = sourceCode.getText(c.value)
+					// 			if (c.key.name === 'required') p['required'] = c.value.raw
+					// 			return p
+					// 		}, {})
+					// 		if (prop.typeAnnotation) {
+					// 			const text = sourceCode.getText(prop.parent.parent)
+					// 			const sourceFile = ts.createSourceFile('', text)
+					// 			const classDeclaration = sourceFile.statements[0]
+					// 			const classDeclarationMembers = classDeclaration.members;
+					// 			const classDeclarationMember = classDeclarationMembers[0]
+					// 			const classDeclarationMemberType = classDeclarationMember.type;
+					// 			const a = classDeclarationMemberType.getText(sourceFile)
+					// 			// const sourceCodes = ts.createSourceFile('test.ts', text)
+					// 			// const program = ts.createProgram({
+					// 			// 	rootNames: ["file.ts"],
+					// 			// 	options: {},
+					// 			// })
+					// 			// const typeChecker = program.getTypeChecker()
+					// 			// const type = typeChecker.getTypeFromTypeNode(sourceCodes)
+
+					// 			debugger
+					// 		}
+					// 		debugger
+					// 	}
+					// }
+				),
 			)
 		},
 	});
