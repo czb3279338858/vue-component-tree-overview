3887de6518075d52860706cccfc8445d05f5cb13
蔡朝勃 <3279338858@qq.com>
2023-04-25 19:20:10 +0800

    1

---
+++ 2.1/vue.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('@vue/cli-service')
+const VueOverviewPlugin = require('../vue-overview-plugin/src/index.js');
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new VueOverviewPlugin({
+        entry: './src/router/routes.ts'
+      })
+    ]
+  }
+})
--- vue-overview-plugin/src/index.js
+++ vue-overview-plugin/src/index.js
@@ -4,10 +4,58 @@
  */
 
 class VueOverviewPlugin {
+  constructor(options = {}) {
+    this.options = options;
+  }
   apply(compiler) {
     compiler.hooks.environment.tap('VueOverviewPlugin', (
     ) => {
-      console.log(compiler.option);
+      const config = compiler.options;
+      const newConfig = {
+        ...config,
+        rules: [
+          {
+            test: /\.(vue)$/,
+            use: [
+              {
+                loader: 'example-loader',
+                options: {},
+              },
+            ],
+          },
+          {
+            test: /\.ts$/,
+            use: [
+              {
+                loader: 'babel-loader'
+              },
+              {
+                loader: 'ts-loader',
+                options: {
+                  transpileOnly: true,
+                  appendTsSuffixTo: [
+                    '\\.vue$'
+                  ],
+                  happyPackMode: false
+                }
+              }
+            ]
+          }
+        ],
+        entry: {
+          routes: {
+            import: path.join(compiler.context, this.options.entry),
+            library: {
+              name: 'routes',
+              type: 'var',
+            }
+          },
+          main: {
+            dependOn: 'routes',
+            import: path.join(__dirname, './main.ts')
+          }
+        },
+      }
     });
   }
 }
