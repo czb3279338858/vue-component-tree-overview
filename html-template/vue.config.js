c3a4ad5ed444981abc59b5570643a57c6d270d16
蔡朝勃 <3279338858@qq.com>
2023-05-18 14:46:56 +0800

    1

--- html-template/vue.config.js
+++ html-template/vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-// const VueOverviewPlugin = require('../vue-overview-plugin/src/index.js');
 module.exports = defineConfig({
   transpileDependencies: true,
+  // 生成map文件
+  productionSourceMap: false,
   css: {
     loaderOptions: {
       postcss: {
@@ -15,10 +16,22 @@ module.exports = defineConfig({
     }
   },
   configureWebpack: {
-    plugins: [
-      // new VueOverviewPlugin({
-      //   entry: './src/router/routes.ts'
-      // })
-    ]
-  }
+    optimization: {
+      // 混淆代码
+      // minimize: false,
+      // 代码分割
+      splitChunks: false
+    }
+  },
+  chainWebpack: config => {
+    const htmlPlugin = config.plugin('html')
+    htmlPlugin.tap(args => {
+      // 不执行js和css注入，js和css通过html模板中的设置内联到模板中
+      args[0].inject = false
+      return args
+    })
+    // 字体文件改为内联
+    config.module.rule('fonts').clear().test(/\.(woff2?|eot|ttf|otf)(\?.*)?$/i).type('asset/inline')
+
+  },
 })
